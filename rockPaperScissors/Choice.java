314ef29a5b26f76c74f1258d546f79498c68aaf5
suac <lrrong2020@gmail.com>
2022-03-26 17:33:38 +0800

    choice equals

--- rockPaperScissors/Choice.java
+++ rockPaperScissors/Choice.java
@@ -45,25 +45,29 @@ public class Choice implements Serializable
 	}
 	
 	public int wins(Choice opponentChoice) {
-		if(opponentChoice.getChoiseName() == GESTURES.ROCK) 
+		if(opponentChoice.getChoiseName().equals(GESTURES.ROCK)) 
 		{
 			if(this.choiceName == GESTURES.PAPER) return 2;
 			else if(this.choiceName == GESTURES.SCISSORS) return 0;
 			else return 1;
 		}
 		
-		else if(opponentChoice.getChoiseName() == GESTURES.PAPER) 
+		else if(opponentChoice.getChoiseName().equals(GESTURES.PAPER)) 
 		{
 			if(this.choiceName == GESTURES.SCISSORS) return 2;
 			else if(this.choiceName == GESTURES.ROCK) return 0;
 			else return 1;
 		}
 		
-		else//opp.getChoiseName() == GESTURES.SCISSORS
+		else if(opponentChoice.getChoiseName().equals(GESTURES.SCISSORS))
 		{
 			if(this.choiceName == GESTURES.ROCK) return 2;
 			else if(this.choiceName == GESTURES.PAPER) return 0;
 			else return 1;
+		}
+		else 
+		{
+			return -1;
 		}
 	}
 }
